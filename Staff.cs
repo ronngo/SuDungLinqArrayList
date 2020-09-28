5da53d18706a9b7d116bb0b4f0ec3bd225b8b245
ronngo <ronngomn23@gmail.com>
2020-09-28 11:57:03 +0700

    first commit

---
+++ Staff.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SuDungLinqArrayList
+{
+    class Staff
+    {
+        public int ID{get; set;}
+        public string Name{get; set;}
+        public int Age{get; set;}
+    }
+}
